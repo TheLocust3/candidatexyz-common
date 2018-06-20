32fbc8a95ad4004a2801e715f05987eb8e2101af
TheLocust3 <jake.kinsella@gmail.com>
2018-06-20 15:25:43 -0400

    Reorganize concerns

--- lib/candidatexyz/common.rb
+++ lib/candidatexyz/common.rb
@@ -1,23 +1,5 @@
-require 'httparty'
-
 module Candidatexyz
-  module Common
-    module Request
-        extend ActiveSupport::Concern
-      
-        def post(url, data)
-            response = HTTParty.post(url, {
-                query: data,
-                headers: {
-                    uid: request.headers['uid'],
-                    client: request.headers['client'],
-                    'access-token': request.headers['access-token']
-                }
-            })
-
-            response.parsed_response
-        end
-    end
-
+  module Concerns
+    require 'concerns/request'
   end
 end
