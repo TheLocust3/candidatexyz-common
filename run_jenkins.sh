dc6e95cd0ad889aee08578fdd93359e99832f50f
TheLocust3 <jake.kinsella@gmail.com>
2018-08-13 12:04:00 -0400

    Add boilerplate jenkins

---
+++ run_jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo Setting up
+
+mkdir ~/.bundle
+touch ~/.bundle/config
+echo BUNDLE_PATH: vendor/bundle > ~/.bundle/config
+
+gem install bundler
+
+cd user-api/
+
+bundle config --local local.candidatexyz-common ../
+bundle install --path vendor/bundle
+
+bundle exec rake db:create
+bundle exec rake db:schema:load
+bundle exec rake db:seed
+
+bundle exec puma -b tcp://127.0.0.1:3003 &
+
+cd ../
+
+bundle install --path vendor/bundle
+
+bundle exec rake db:create
+bundle exec rake db:schema:load
+bundle exec rake db:seed
+
+echo Running tests
+
+./bin/test
